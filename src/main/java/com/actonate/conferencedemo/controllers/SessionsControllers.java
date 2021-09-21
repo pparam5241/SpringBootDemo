8344a08a9a2e72f069d32dfb1662656aa2d69d29
Param <52886407+pparam5241@users.noreply.github.com>
2021-09-21 21:46:35 +0530

    Add files via upload

--- src/main/java/com/actonate/conferencedemo/controllers/SessionsControllers.java
+++ src/main/java/com/actonate/conferencedemo/controllers/SessionsControllers.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/sessions")
+@RequestMapping("/sessions")
 public class SessionsControllers {
     @Autowired
     private SessionRepository sessionRepository;
